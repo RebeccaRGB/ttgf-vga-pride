66202344cb800335f5cd5af243adc2f19b30b679
RebeccaRGB <beckiejo@gmail.com>
2026-09-24 03:14:48 -0700

    bbbbbbbbbb

--- src/project.v
+++ src/project.v
@@ -60,10 +60,10 @@ module tt_um_rebeccargb_vga_pride (
       reset_latch[0] <= ui_in[7] | ui_in[5];
     end else if (reset_latch[2]) begin
       case (reset_latch[1:0])
-        3: counter <= 0;
-        2: counter <= (counter < max) ? (counter + 1) : 0;
-        1: counter <= (counter > 0) ? (counter - 1) : max;
-        0: counter <= uio_in[6:0];
+        2'b11: counter <= 0;
+        2'b10: counter <= (counter < max) ? (counter + 1) : 0;
+        2'b01: counter <= (counter > 0) ? (counter - 1) : max;
+        2'b00: counter <= uio_in[6:0];
       endcase
       reset_latch <= 0;
     end
